7e98d365bd5ed7d022bb8efc32ea3d6bb430de20
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2018-12-28 13:52:23 -0500

    Rename WordList.start to word_list
    This captures the intent better.

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -2,7 +2,7 @@ defmodule Dictionary do
 
   alias Dictionary.WordList
 
-  defdelegate start, to: WordList
+  defdelegate start, to: WordList, as: :word_list
   defdelegate random_word(words), to: WordList
 
 end
